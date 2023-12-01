9ffc49801b1288ac3c36ea079a4457f61f2d4987
Pablo <holaje02@gmail.com>
2023-12-01 01:41:32 -0400

    Se agregaron las funciones de menu de opciones para los tres colegios

--- main.cpp
+++ main.cpp
@@ -1,6 +1,109 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+
+
+void mostrarMenuColegio1() {
+    int curso;
+    do {
+        cout << "\nCursos disponibles en La Salle " << endl;
+        cout << "1. Curso 1" << endl;
+        cout << "2. Curso 2" << endl;
+        cout << "3. Curso 3" << endl;
+        cout << "4. Curso 4" << endl;
+        cout << "5. Curso 5" << endl;
+        cout << "6. Curso 6" << endl;
+        cout << "0. Volver al menú principal" << endl;
+
+        cout << "Ingrese el número de la opción deseada: ";
+        cin >> curso;
+
+        switch (curso) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                cout << "Ha seleccionado el Curso " << curso << " en La Salle" << endl;
+                break;
+            case 0:
+                cout << "Volviendo al menú principal." << endl;
+                break;
+            default:
+                cout << "Opción no válida. Por favor, ingrese un número válido." << endl;
+        }
+    } while (curso != 0);
+}
+
+void mostrarMenuColegio2() {
+  int curso;
+    do {
+        cout << "\nCursos disponibles en Santo " << endl;
+        cout << "1. Curso 1" << endl;
+        cout << "2. Curso 2" << endl;
+        cout << "3. Curso 3" << endl;
+        cout << "4. Curso 4" << endl;
+        cout << "5. Curso 5" << endl;
+        cout << "6. Curso 6" << endl;
+        cout << "0. Volver al menú principal" << endl;
+
+        cout << "Ingrese el número de la opción deseada: ";
+        cin >> curso;
+
+        switch (curso) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                cout << "Ha seleccionado el Curso " << curso << " en La Salle" << endl;
+                break;
+            case 0:
+                cout << "Volviendo al menú principal." << endl;
+                break;
+            default:
+                cout << "Opción no válida. Por favor, ingrese un número válido." << endl;
+        }
+    } while (curso != 0);
+
+}
+
+void mostrarMenuColegio3() {
+      int curso;
+    do {
+        cout << "\nCursos disponibles en La Vento " << endl;
+        cout << "1. Curso 1" << endl;
+        cout << "2. Curso 2" << endl;
+        cout << "3. Curso 3" << endl;
+        cout << "4. Curso 4" << endl;
+        cout << "5. Curso 5" << endl;
+        cout << "6. Curso 6" << endl;
+        cout << "0. Volver al menú principal" << endl;
+
+        cout << "Ingrese el número de la opción deseada: ";
+        cin >> curso;
+
+        switch (curso) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                cout << "Ha seleccionado el Curso " << curso << " en La Salle" << endl;
+                break;
+            case 0:
+                cout << "Volviendo al menú principal." << endl;
+                break;
+            default:
+                cout << "Opción no válida. Por favor, ingrese un número válido." << endl;
+        }
+    } while (curso != 0);
+
+}
 
 int main() {
     int opcion;
@@ -8,9 +111,9 @@ int main() {
     do {
         cout << "Colegios disponibles: " << endl;
 
-        cout << "2. Colegio 1" << endl;
-        cout << "3. Colegio 2" << endl;
-        cout << "4. Colegio 3" << endl;
+        cout << "1. Colegio La Salle" << endl;
+        cout << "2. Colegio Santo" << endl;
+        cout << "3. Colegio Vento" << endl;
         cout << "0. Salir" << endl;
 
         cout << "Ingrese el número de la opción deseada: ";
@@ -19,13 +122,16 @@ int main() {
         switch (opcion) {
             
             case 1:
-                cout << "Ha seleccionado 'Colegio 1'" << endl;
+                cout << "Ha seleccionado La Salle " << endl;
+                mostrarMenuColegio1();
                 break;
             case 2:
-                cout << "Ha seleccionado 'Colegio 2'" << endl;
+                cout << "Ha seleccionado  Santo " << endl;
+                mostrarMenuColegio2();
                 break;
             case 3:
-                cout << "Ha seleccionado 'Colegio 3'" << endl;
+                cout << "Ha seleccionado  Ventos" << endl;
+                mostrarMenuColegio3();
                 break;
             case 0:
                 cout << "Saliendo del programa. ¡Hasta luego!" << endl;
